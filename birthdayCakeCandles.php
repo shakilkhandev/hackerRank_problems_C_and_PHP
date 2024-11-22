546fd6e633570f4ede91d315fdc603bcb0ab5e61
Shakil Khan <60468711+shakilkhandev@users.noreply.github.com>
2024-11-23 05:30:44 +0600

    Create birthdayCakeCandles.php
    here it's about finding the max height candles count form the given array.

---
+++ birthdayCakeCandles.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * Complete the 'birthdayCakeCandles' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY candles as parameter.
+ */
+
+function birthdayCakeCandles($candles) {
+    // Write your code here
+    
+    $maxheight = max($candles);
+    $count = 0 ;
+    foreach($candles as $candle){
+        if($maxheight == $candle){$count++;}
+    }
+     return $count ;
+}
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$candles_count = intval(trim(fgets(STDIN)));
+
+$candles_temp = rtrim(fgets(STDIN));
+
+$candles = array_map('intval', preg_split('/ /', $candles_temp, -1, PREG_SPLIT_NO_EMPTY));
+
+$result = birthdayCakeCandles($candles);
+
+fwrite($fptr, $result . "\n");
+
+fclose($fptr);
